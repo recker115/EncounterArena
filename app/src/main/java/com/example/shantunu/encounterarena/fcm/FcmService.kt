af5700db29e91db546f13730eacdd2285a9b389e
recker <sursantanu614@gmail.com>
2019-01-16 03:21:58 -0800

    addition of multiple notifications is completed

--- app/src/main/java/com/example/shantunu/encounterarena/fcm/FcmService.kt
+++ app/src/main/java/com/example/shantunu/encounterarena/fcm/FcmService.kt
@@ -1,6 +1,5 @@
 package com.example.shantunu.encounterarena.fcm
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
